c042c77e0d40e4af39487bc07195e6e9cf177fdc
dlri <chenchuang.sy@163.com>
2018-05-15 16:41:02 +0800

    数据库

---
+++ src/detection_details.sql
@@ -0,0 +1,5 @@
+INSERT INTO `detection_details` VALUES (1, 'IN0:A1', 35.28, 50.55, 0.41, 0, 0, 4, '合格', '', 11);
+INSERT INTO `detection_details` VALUES (2, 'IN0:A1', 35.28, 50.55, 0.41, 0, 0, 4, '合格', '', 11);
+INSERT INTO `detection_details` VALUES (3, 'IN0:A1', 35.28, 50.55, 0.41, 0, 0, 4, '合格', '', 11);
+INSERT INTO `detection_details` VALUES (4, 'IN0:A2', 32.53, 50.47, 0.43, 0, 0, 4, '合格', '', 11);
+INSERT INTO `detection_details` VALUES (5, 'IN0:B1', 40.04, 53.62, 0.41, 0, 0, 4, '合格', '', 11);
---
+++ src/detection_record.sql
@@ -0,0 +1,4 @@
+INSERT INTO `detection_record` VALUES (10, 'H63-2304', '三级', '5-15-3', '5-15-1', '2018-3-3 09:40:41', '跑合台', 'RUN001', 'D://test', 'cc.pdf', 'cc.bgm');
+INSERT INTO `detection_record` VALUES (11, 'H63-2304', '三级', '5-15-3', '5-15-1', '2018-3-3 09:40:41', '跑合台', 'RUN001', 'D://test', 'cc.pdf', 'cc.bgm');
+INSERT INTO `detection_record` VALUES (12, 'H63-2304', '三级', '5-15-3', '5-15-1', '2018-3-3 09:40:41', '跑合台', 'RUN001', 'D://test', 'cc.pdf', 'cc.bgm');
+INSERT INTO `detection_record` VALUES (13, 'H63-2304', '三级', '5-15-3', '5-15-1', '2018-3-3 09:40:41', '跑合台', 'RUN001', 'D://test', 'cc.pdf', 'cc.bgm');
